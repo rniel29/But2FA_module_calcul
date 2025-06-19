27214db07b061ad0d188a8b43deccfcc3d88858c
Maryam <maryam.koundi@hotmail.com>
2025-06-19 10:21:17 +0200

    ajouter du lien mot de passe oublié

---
+++ src/mdp_oublier.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mot de passe oublié</title>
+    </head>
+    <body>
+        <p>Page en construction, veuillez entrer en contact avec notre administrateur systeme pour connaitre votre mot de passe</p>
+    </body>
+</html>
